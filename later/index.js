fa835cebdfb0c9392703f64715ab6d0dad84649a
unknown <QinYuChao>
2019-11-19 18:12:56 +0800

    dev

--- later/index.js
+++ later/index.js
@@ -6,16 +6,32 @@ const read = require('node-readability')
 const url = 'http://www.manning.com/cantelon2/'
 
 read(url,(err,result)=>{
-    Article.create({title:result.title, content:result.content}),
-    (err,article) => {
+    if(err || !result) res.status(500).send('Error downloading article');
+    console.log(result.title)
+    // Article.create({title:result.title, content:result.content}),
+    // (err,article) => {
 
-    }
+    // }
 })
+
 
 app.set('port',process.env.PORT || 3000)
 
 app.use(bodyParser.json()); //支持编码为json 的请求消息体
 app.use(bodyParser.urlencoded({ extended:true })); //支持编码为表单的请求消息体
+
+app.post('/articles', (req, res, next) => {
+    //从post 消息体中取得url
+    const url = req.body.url;
+    console.log(req.body.url)
+    read (url, (err, result) => {
+        if(err || !result){res.status(500).send('Error downloading article');}
+        Article.create({title:result.title, content:result.content}, (err, article) => {
+            if (err) return next(err);
+            res.send('ok');
+        })
+    })
+})
 
 app.get('/articles', (req, res, next) => {
     Article.all((err, articles) => {
